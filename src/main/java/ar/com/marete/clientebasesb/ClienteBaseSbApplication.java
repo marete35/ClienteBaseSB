11840d368b0babc3fdede558db2468b27f46030d
Mario <lic.galarza@hotmail.com>
2022-10-13 21:57:09 -0300

    Primer Commit

---
+++ src/main/java/ar/com/marete/clientebasesb/ClienteBaseSbApplication.java
@@ -0,0 +1,13 @@
+package ar.com.marete.clientebasesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteBaseSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteBaseSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/marete/clientebasesb/ClienteBaseSbApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.marete.clientebasesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteBaseSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
